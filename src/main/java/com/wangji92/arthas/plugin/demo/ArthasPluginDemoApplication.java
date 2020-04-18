bf8c7ac25b33e705a1e59e870f31b6679dde1434
wangji <983433479@qq.com>
2020-04-18 23:38:11 +0800

    增加logger 动态更新的功能

--- src/main/java/com/wangji92/arthas/plugin/demo/ArthasPluginDemoApplication.java
+++ src/main/java/com/wangji92/arthas/plugin/demo/ArthasPluginDemoApplication.java
@@ -2,8 +2,10 @@ package com.wangji92.arthas.plugin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ArthasPluginDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/wangji92/arthas/plugin/demo/controller/LoggerDemo.java
@@ -0,0 +1,28 @@
+package com.wangji92.arthas.plugin.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 体验一下修改logger的等级
+ *
+ * @author 汪小哥
+ * @date 18-04-2020
+ */
+@Slf4j
+@Component
+public class LoggerDemo {
+
+    /**
+     * logger --name com.wangji92.arthas.plugin.demo.controller.LoggerDemo --level trace -c 18b4aac2
+     */
+    @Scheduled(initialDelay = 1000, fixedRate = 5000)
+    public void sample() {
+        log.trace("....trace");
+        log.debug("......debug");
+        log.warn("........warn");
+        log.info("..........info");
+        log.error("............error");
+    }
+}
