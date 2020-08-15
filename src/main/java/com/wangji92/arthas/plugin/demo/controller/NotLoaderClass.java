94d504980207dbbb6a91e8077db93d3ad75ac887
wangji <983433479@qq.com>
2020-08-15 20:12:02 +0800

    手动加载class

---
+++ src/main/java/com/wangji92/arthas/plugin/demo/controller/NotLoaderClass.java
@@ -0,0 +1,12 @@
+package com.wangji92.arthas.plugin.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 汪小哥
+ * @date 15-08-2020
+ */
+@Slf4j
+public class NotLoaderClass {
+    public static String NO_LOAD = "not load";
+}
